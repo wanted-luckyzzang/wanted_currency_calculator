5aa2f1ea90336310c9cb1b412584a1f41794557d
Yena-Yun <cheryleduart@gmail.com>
2022-01-25 23:03:50 +0900

    style: 전체적인 뷰 디자인

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './reset.css';
+import styled from 'styled-components';
 import Calculator2 from './components/Calculator2';
-// import Test from './utils/Test';
 
 function App() {
-  return <Calculator2 />;
+  return (
+    <Wrapper>
+      <Calculator2 />
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: url('img/background.jpg') no-repeat;
+  background-size: cover;
+`;
 
 export default App;
