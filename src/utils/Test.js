713a253bf83cf8a832cd29602f7d28fe281b72b0
Yena-Yun <cheryleduart@gmail.com>
2022-01-25 15:26:58 +0900

    add: 드롭다운에 hover 시 리스트 렌더링 + 각 리스트메뉴에 hover 시 배경색 변화

--- src/utils/Test.js
+++ src/utils/Test.js
@@ -3,12 +3,10 @@ import formatDate from './formatDate';
 import { formatMoney, roundToTwo } from './formatMoney';
 
 function Test() {
-  return (
-    <div>
-      <div>{formatDate()}</div>
-      <div>{formatMoney(roundToTwo(1000000.488))}</div>
-    </div>
-  );
+  <div>
+    <div>{formatDate()}</div>
+    <div>{formatMoney(roundToTwo(1000000.488))}</div>
+  </div>;
 }
 
 export default Test;
